c6a42945588ed705c4ce38e95e0cafd7ec79a663
jkcomt <jkcomt@gmail.com>
2019-07-15 00:15:01 -0500

    material app - scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,25 @@ void main() => runApp(MyAnimeApp());
 class MyAnimeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text("Hola Mundo",
-          style: TextStyle(
-            color: Colors.black
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Center(child: Text("My Anime App")),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Text(
+              "Hola Mundo",
+              style: TextStyle(color: Colors.black,fontSize: 20),
+              textDirection: TextDirection.ltr,
+            ),
           ),
-          textDirection: TextDirection.ltr,
         ),
       ),
     );
   }
 }
-
