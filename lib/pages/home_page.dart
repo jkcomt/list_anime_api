1e6f796d8baf405f4828073575a4879fb52b01c5
jkcomt <jkcomt@gmail.com>
2019-07-18 21:43:33 -0500

    basic list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
-import 'dart:convert';
-
-import 'package:list_anime_api/model/anime.dart';
 
 class HomePage extends StatelessWidget {
   
